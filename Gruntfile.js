987e5fefab4d8804d960ebb9076081d3ffdc463a
Zack Elliott <hubridnoxx@gmail.com>
2014-09-19 01:34:58 -0400

    HA

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,6 +6,7 @@
 // 'test/spec/{,*/}*.js'
 // use this if you want to recursively match all subfolders:
 // 'test/spec/**/*.js'
+// END
 
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
